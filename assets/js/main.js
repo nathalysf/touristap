b0c7963fe3d2cff2a80fe522783f0bbf7f707aa4
Nathaly <nathaly@laboratoria.la>
2016-07-18 00:07:56 -0500

    countdown js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,126 +1,64 @@
-(function($) {
-  $.fn.countdown = function(options, callback) {
-    //custom 'this' selector
-    thisEl = $(this); 
-  
-    // array of custom settings
-    var settings = { 
-      'date': null,
-      'format': null
-    };
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
-    // append the settings array to options
-    if(options) {
-      $.extend(settings, options);
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
     }
-   
-    //create the countdown processing function
-    function countdown_proc() {
-    var eventDate = Date.parse(settings.date) / 1000;
-    var currentDate = Math.floor($.now() / 1000);
-    
-    if(eventDate <= currentDate) {
-    callback.call(this);
-    clearInterval(interval);
-    }
-      
-    var seconds = eventDate - currentDate;
-    
-    var days = Math.floor(seconds / (60 * 60 * 24)); 
-    //calculate the number of days
-    
-    seconds -= days * 60 * 60 * 24; 
-    //update the seconds variable with number of days removed
-    
-    var hours = Math.floor(seconds / (60 * 60));
-    seconds -= hours * 60 * 60; 
-    //update the seconds variable with number of hours removed
-    
-    var minutes = Math.floor(seconds / 60);
-    seconds -= minutes * 60; 
-    //update the seconds variable with number of minutes removed
-    
-    //conditional statements
-    if (days == 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
-		    if (hours == 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
-		    if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
-		    if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
-    
-    //logic for the two_digits ON setting
-    if(settings.format == "on") {
-        days = (String(days).length >= 2) ? days : "0" + days;
-        hours = (String(hours).length >= 2) ? hours : "0" + hours;
-        minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
-        seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
-    }
-    
-    //update the countdown's html values.
-    thisEl.find(".days").text(days);
-    thisEl.find(".hours").text(hours);
-    thisEl.find(".minutes").text(minutes);
-    thisEl.find(".seconds").text(seconds);
   }
 
-	//run the function
-	countdown_proc();
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
 
-	//loop the function
-	interval = setInterval(countdown_proc, 1000);
-  };
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
 
-}) (jQuery);
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv-1', deadline);
+
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv-2', deadline);
+
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv-3', deadline);
 
 
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv-movil', deadline);
 
-//Provide the plugin settings
-$("#countdown-d1").countdown({
-      //The countdown end date
-      date: "20 july 2016 01:00:00",
-      
-      // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
-      format: "on"
-      }, 
-  
-      function() {
-      // This will run when the countdown ends
-       alert("end!");
-       });
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv1-movil', deadline);
 
-$("#countdown-d2").countdown({
-      //The countdown end date
-      date: "20 july 2016 01:00:00",
-      
-      // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
-      format: "on"
-      }, 
-  
-      function() {
-      // This will run when the countdown ends
-       alert("end!");
-       });
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv2-movil', deadline);
 
-$("#countdown-d3").countdown({
-      //The countdown end date
-      date: "20 july 2016 01:00:00",
-      
-      // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
-      format: "on"
-      }, 
-  
-      function() {
-      // This will run when the countdown ends
-       alert("end!");
-       });
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv3-movil', deadline);
 
-$("#countdown-d4").countdown({
-      //The countdown end date
-      date: "20 july 2016 01:00:00",
-      
-      // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
-      format: "on"
-      }, 
-  
-      function() {
-      // This will run when the countdown ends
-       alert("end!");
-       });
